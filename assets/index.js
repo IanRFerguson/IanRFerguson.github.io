dfb694da01c59d774f74fcbe2a190973c1211b92
Ian <47256454+IanRFerguson@users.noreply.github.com>
2021-01-26 08:04:57 -0800

    Update index.js

--- assets/index.js
+++ assets/index.js
@@ -4,5 +4,5 @@ document.body.className = 'fade';
 document.addEventListener('DOMContentLoaded', () => {
   window.setTimeout(function() {
     document.body.className = '';
-  }, 100);
+  }, 300);
   });
